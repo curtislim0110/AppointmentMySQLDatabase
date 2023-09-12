c2f347fa896d317180b6e15f522e9a2c2e1b42d4
Curtis Lim <curtislim1983@gmail.com>
2023-09-12 15:04:25 -0600

    moved main to /src and fixed DAO and model view controller

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import DAO.JDBC;
+import java.util.Locale;
 
 public class Main extends Application {
-
-    // Locale.setDefault(new Locale("fr"));
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
-        primaryStage.setTitle("anothertest");
+        primaryStage.setTitle("3test");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
+        // Setting and displaying the Locale to french for testing only
+        // Locale.setDefault(new Locale("fr"));
+        Locale new_locale = Locale.getDefault();
+        System.out.println("Current system locale: " + new_locale);
+
         JDBC.makeConnection();
         launch(args);
     }
