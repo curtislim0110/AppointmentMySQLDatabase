d27fcec5aa10a67279eb8f1a2b65713b5c84e514
Curtis Lim <curtislim1983@gmail.com>
2023-09-18 23:39:57 -0600

    combo boxes for start and end times fully completed. add appointment times is next

--- src/helper/timeHelper.java
+++ src/helper/timeHelper.java
@@ -9,23 +9,28 @@ public class timeHelper {
 
     // This function is used to display potential appointment times in a user's local time zone, based on EST business hours of 8:00 to 22:00.
 
-    public static ObservableList<LocalTime> appointmentHoursEST() {
+    public static ObservableList<LocalTime> appointmentHoursEST(LocalTime businessHourStart) {
 
         ObservableList<LocalTime> timeList = FXCollections.observableArrayList();
-        ZonedDateTime businessZDT = ZonedDateTime.of(LocalDate.now(), LocalTime.of(8, 0), ZoneId.of("America/New_York"));
-        ZonedDateTime localZDT = ZonedDateTime.ofInstant(businessZDT.toInstant(), ZoneId.systemDefault());
-        int localStartingHour = localZDT.getHour();
-        int totalHours = localStartingHour + 13;
-        int midnightOrGreater = 0;
 
-        for(int i = localStartingHour; i <= totalHours; i++) {
-            if(i<24) {
-                timeList.add(LocalTime.of(i,0));
-            }
-            if(i>23) {
-                timeList.add(LocalTime.of(midnightOrGreater,0));
-                midnightOrGreater += 1;
-            }
+        // Convert starting business hours of 8am EST into a ZonedDateTime object
+        ZonedDateTime businesszonetime = ZonedDateTime.of(LocalDate.now(), businessHourStart, ZoneId.of("America/New_York"));
+        System.out.println(businesszonetime);
+        // Convert business hours ZonedDateTime discovered above into the system default time zone
+        ZonedDateTime localzonetime = businesszonetime.withZoneSameInstant(ZoneId.systemDefault());
+        System.out.println(localzonetime);
+
+        // find the first hour to start the list of appointment times
+        int firstHour = localzonetime.getHour();
+
+        // find the final hour in which to stop listing appointment times, based on 30 minute appointment times and a 14 hour workday
+        int finalhour = firstHour + 27;
+
+        // iterate over time objects and add them to timeList for combo box display
+        for(int i = firstHour; i <= finalhour; i++) {
+            LocalTime newLocalTime = localzonetime.toLocalTime();
+            timeList.add(newLocalTime);
+            localzonetime = localzonetime.plusMinutes(30);
         }
         return timeList;
     }
