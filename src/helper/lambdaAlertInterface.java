b9536399edd15d573ff9dc3ff0e4661d96bb4988
Curtis Lim <curtislim1983@gmail.com>
2023-09-20 21:23:11 -0600

    javadoc generated and some errors in lambda @param javadoc removed

--- src/helper/lambdaAlertInterface.java
+++ src/helper/lambdaAlertInterface.java
@@ -4,8 +4,8 @@ package helper;
  * Interface for Lambda Expression #1 used in the customers and appointments controllers. This Lambda Expression
  * results in code that is slightly shorter and easier to read.  See AppointmentsController and CustomersController
  * for implementation of lambdaAlertInterface and additional details.
- * @param alertNumber ia used to select between different types of alerts
  */
+
 public interface lambdaAlertInterface {
     void lambdaAlertMethod(int alertNumber);
 }
--- src/helper/lambdaTextInterface.java
+++ src/helper/lambdaTextInterface.java
@@ -6,7 +6,6 @@ import java.io.IOException;
  * Interface for Lambda Expression #2 used in LoginScreenController.java. This Lambda Expression is
  * slightly shorter, and could be implemented in multiple classes as needed while also improving readability
  * of code. This lambda also handles IOExceptions.
- * @param trueOrFalse is a boolean parameter that can be used in an if/else statement inside the method
  */
 public interface lambdaTextInterface {
     void lambdaBoolean(boolean trueOrFalse) throws IOException;
